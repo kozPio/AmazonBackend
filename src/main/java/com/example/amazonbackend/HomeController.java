fcef71973075ec50cc6bfb04219e4018bbe7a86a
Piotr Kozłowski <pitrx77@gmail.com>
2021-11-27 22:44:01 +0100

    Learning JAva with spring boot part3 (fixing displaying homepage)

--- src/main/java/com/example/amazonbackend/HomeController.java
+++ src/main/java/com/example/amazonbackend/HomeController.java
@@ -2,14 +2,14 @@ package com.example.amazonbackend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("home")
+    @RequestMapping("/home")
     public String home() {
         System.out.println("Home");
-        return "index.jsp";
+        return "home";
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -5,13 +5,12 @@
   Time: 22:24
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8"  %>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <html>
   <head>
     <title>$Title$</title>
   </head>
   <body>
-    hello
-
+  $END$
   </body>
 </html>
