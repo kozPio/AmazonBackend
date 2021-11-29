d7725091df570785d30ebe2590c96fc6bf36c9e5
Piotr Kozłowski <pitrx77@gmail.com>
2021-11-29 23:18:01 +0100

    Learning JAva with spring boot part4 (figuring out passing params to html file from controller)

--- src/main/java/com/example/amazonbackend/HomeController.java
+++ src/main/java/com/example/amazonbackend/HomeController.java
@@ -1,15 +1,22 @@
 package com.example.amazonbackend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home() {
-        System.out.println("Home");
+    public String home(@RequestParam(name="name", required = false, defaultValue = "Ahmed") String name, Model model) {
+
+        model.addAttribute(name);
         return "home";
     }
 }
